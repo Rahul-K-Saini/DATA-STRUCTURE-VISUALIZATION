103848570dba307965b9ee133761f3e0476f7e1a
Rahul Saini <rahul545436@gmail.com>
2023-08-02 22:58:41 +0530

    Almost Completed it fixed some issues

--- BST/BST.js
+++ BST/BST.js
@@ -1,63 +1,152 @@
+"use strict";
+
+/* Global Variables*/
+
+const input = document.querySelector('input')
+const treeContainer = document.getElementById('tree_cont')
+const preButton = document.getElementById('preorder')
+const postButton = document.getElementById('postorder')
+const inButton = document.getElementById('inorder')
+const traversalContainer = document.getElementById('traversals')
+const whichTraversalSpan = document.getElementById('whichTraversal')
+const traversalValues = document.getElementById('tarversalValues')
+const errorContainer = document.getElementById('errorMsg')
+const errorText = document.getElementById('errMessage')
+
+
+function createEle(type, text, top, right, bottom, left) {
+    const node = document.createElement(type);
+    node.setAttribute("class", "node");
+    node.textContent = text;
+    node.style.margin = `${top}px ${right}px ${bottom}px ${left}px`;
+    node.style.backgroundColor = '#56a4e4';
+    node.style.color = 'black';
+    return node
+}
+
 class BinarySearchTree {
     constructor() {
         this.root = null;
     }
 
     isEmpty() {
-        return this.root === null;
+        return this.root == null
     }
 
     insert(value) {
-        this.root = this._insert(value, this.root);
+        this.root = this._insert(this.root, value);
     }
-
-    _insert(value, node) {
-        if (node === null) {
+    _insert(node, value) {
+        if (node == null) {
             return new TreeNode(value);
         }
-        if (value < node.data) {
-            node.left = this._insert(value, node.left);
+        if (value < node.value) {
+            node.left = this._insert(node.left, value)
         }
-        if (value > node.data) {
-            node.right = this._insert(value, node.right);
+        if (value > node.value) {
+            node.right = this._insert(node.right, value);
         }
         return node;
+
     }
 
     display() {
-        this._display(this.root, null, false);
+        treeContainer.innerHTML = "";
+        this._display(this.root, 20, 0, 0, 400)
     }
-
-    _display(node, parent, isRightChild) {
-        if (node === null) {
+    _display(node, top, right, bottom, left) {
+        if (node == null) {
             return;
         }
-        let details = "";
-        if (parent !== null) {
-            details += (isRightChild ? "Right" : "Left") + " Child of " + parent.data + ": ";
-        } else {
-            details += "Root Node: ";
+        let ele = createEle('div', node.value, top, right, bottom, left)
+        treeContainer.appendChild(ele);
+        this._display(node.left, top + 60, right + 30, bottom, left - 50)
+        this._display(node.right, top + 60, right - 30, bottom, left + 50)
+    }
+    preorder(node) {
+        if (node == null) {
+            return
         }
-        console.log(details + node.data);
-        this._display(node.left, node, false);
-        this._display(node.right, node, true);
+        traversalValues.textContent += `${node.value},  `
+        this.preorder(node.left)
+        this.preorder(node.right)
+
+    }
+    postorder(node) {
+        if (node == null) {
+            return
+        }
+        this.postorder(node.left)
+        this.postorder(node.right)
+        traversalValues.textContent += `${node.value},  `
+    }
+    inorder(node) {
+        if (node == null) {
+            return
+        }
+        this.inorder(node.left)
+        traversalValues.textContent += `${node.value},   `
+        this.inorder(node.right)
     }
 
-    populate(nums) {
-        for (let n of nums) {
-            this.insert(n);
-        }
-    }
 }
-
 class TreeNode {
-    constructor(data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
+    constructor(value) {
+        this.value = value
+        this.right = null
+        this.left = null
     }
 }
 
-const bst = new BinarySearchTree();
-bst.populate([5,4,3,6,8,7])
-bst.display();
+const tree = new BinarySearchTree()
+
+function preOrder() {
+    if (input.value.trim().length == 0) {
+        showError("No node to traverse")
+        return
+    }
+    whichTraversalSpan.textContent = "Preorder Traversal: "
+    traversalValues.textContent = ""
+    tree.preorder(tree.root);
+}
+
+function postOrder() {
+    if (input.value.trim().length == 0) {
+        showError("No node to traverse")
+        return
+    }
+    whichTraversalSpan.textContent = "Postorder Traversal: "
+    traversalValues.textContent = ""
+    tree.postorder(tree.root);
+}
+
+function inOrder() {
+    if (input.value.trim().length == 0) {
+        showError("No node to traverse")
+        return
+    }
+    whichTraversalSpan.textContent = "Inorder Traversal: "
+    traversalValues.textContent = ""
+    tree.inorder(tree.root);
+}
+
+function showError(message) {
+    errorContainer.style.display = 'flex';
+    errorText.innerHTML = `<b>Enter value first! </b> ${message}`;
+}
+
+function makeTree() {
+    tree.root = null
+    let values = input.value.trim();
+    if (values.length == 0) {
+        showError("Looks like you did not enter any number for BST Visualizer to display")
+    }
+    else {
+        let valuesArr = values.split(" ")
+        valuesArr.forEach((x) => {
+            tree.insert(Number(x))
+        })
+        tree.display();
+    }
+
+}
